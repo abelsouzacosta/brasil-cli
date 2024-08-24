c30a6e92c838d0f1b171e91227409dee64df4a1f
abelsouzacosta <abelsouzacostajunior@proton.me>
2024-08-24 20:29:19 -0300

    [chore]: adding tabulation

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import typer
 
 from modules.cep.service import CepService
+from infra.utils.tabulate import Tabulator
 
 cli = typer.Typer()
 
 
 @cli.command()
-def cep(cep: str, api_cep_version: int = 1):
+def cep(cep: str, api_cep_version: int = 1, tabulate: bool = False):
     result = CepService().execute(cep, api_cep_version)
-    print(f"{result}")
+    if tabulate:
+        Tabulator("CEP").execute(result)
+    else:
+        print(f"{result}")
 
 
 if __name__ == "__main__":
